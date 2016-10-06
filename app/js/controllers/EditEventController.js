0a3cd852f8768657e18ab2ed5f808906a4e24fa1
marco-mamani-tierconnect-com <natmikhir@gmail.com>
2016-10-06 17:29:49 -0400

    Demo: Two way binding 5:45

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+eventsApp.contoller('EditEventController',
+    function EditEventController($scope) {
+
+    }
+);
--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -22,7 +22,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Rob Smith',
-                    duration: '1hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives!',
                     upVoteCount: 0
@@ -30,7 +30,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scope for fun and profit',
                     creatorName: 'Jon Doe',
-                    duration: '30 mins',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
                     upVoteCount: 0
@@ -38,7 +38,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well behaved Controller',
                     creatorName: 'Jane Dow',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
                     upVoteCount: 0
