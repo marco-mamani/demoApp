1a9d460c470871c367d07570a545f9cfe76cb428
marco-mamani-tierconnect-com <natmikhir@gmail.com>
2016-11-30 14:43:37 -0400

    $resource service - part2 (using premises then...catch)

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,7 +3,11 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData, $log) {
         $scope.sortorder = 'name';
-        $scope.event = eventData.getEvent();
+        $scope.event = eventData.getEvent()
+            .$promise
+            .then(function (event) { $scope.event = event; console.log(event); })
+            .catch(function (response) { console.log(response); }
+        );
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
